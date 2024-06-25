1de3088a69a0376ac32fc8c3a8dc14576849b752
cwjcw <zhuce@cuixiaoyuan.cn>
2024-06-25 15:37:26 +0800

    update

--- learning/03_双均线策略.py
+++ learning/03_双均线策略.py
@@ -5,8 +5,9 @@ from pandas import Series
 import numpy as np
 import json
 
-df = pd.read_csv(r'./learning/000001.csv')
-
+# 数据来源于"01_股票预处理.py"
+df = pd.read_csv(r'./learning/000001.csv') 
+# print(df)
 
 
 
